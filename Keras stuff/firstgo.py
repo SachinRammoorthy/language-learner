55602c24ffbfa6addbe231274b20155e30344fe6
Shashank Rammoorthy <shashankr.blr@gmail.com>
2018-01-20 16:43:12 +0530

    Added Keras model, still need to make it mobile-ready

--- Keras stuff/firstgo.py
+++ Keras stuff/firstgo.py
@@ -9,11 +9,9 @@ from keras import backend as K
 # dimensions of our images.
 img_width, img_height = 150, 150
 
-train_data_dir = 'data/train'
-validation_data_dir = 'data/validation'
+train_data_dir = 'flower_photos'
 nb_train_samples = 2000
-nb_validation_samples = 800
-epochs = 50
+epochs = 10
 batch_size = 16
 
 if K.image_data_format() == 'channels_first':
@@ -23,26 +21,26 @@ else:
 
 model = Sequential()
 model.add(Conv2D(32, (3, 3), input_shape=input_shape))
-model.add(Activation('relu'))
+model.add(Activation('softmax'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
 model.add(Conv2D(32, (3, 3)))
-model.add(Activation('relu'))
+model.add(Activation('softmax'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
 model.add(Conv2D(64, (3, 3)))
-model.add(Activation('relu'))
+model.add(Activation('softmax'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
 model.add(Flatten())
 model.add(Dense(64))
-model.add(Activation('relu'))
+model.add(Activation('softmax'))
 model.add(Dropout(0.5))
-model.add(Dense(1))
-model.add(Activation('sigmoid'))
+model.add(Dense(7))
+model.add(Activation('softmax'))
 
-model.compile(loss='binary_crossentropy',
-              optimizer='rmsprop',
+model.compile(loss='categorical_crossentropy',
+              optimizer='adam',
               metrics=['accuracy'])
 
 # this is the augmentation configuration we will use for training
@@ -60,19 +58,14 @@ train_generator = train_datagen.flow_from_directory(
     train_data_dir,
     target_size=(img_width, img_height),
     batch_size=batch_size,
-    class_mode='binary')
+    class_mode='categorical')
 
-validation_generator = test_datagen.flow_from_directory(
-    validation_data_dir,
-    target_size=(img_width, img_height),
-    batch_size=batch_size,
-    class_mode='binary')
+
 
 model.fit_generator(
     train_generator,
     steps_per_epoch=nb_train_samples // batch_size,
-    epochs=epochs,
-    validation_data=validation_generator,
-    validation_steps=nb_validation_samples // batch_size)
+    epochs=epochs
+    )
 
 model.save_weights('first_try.h5')
