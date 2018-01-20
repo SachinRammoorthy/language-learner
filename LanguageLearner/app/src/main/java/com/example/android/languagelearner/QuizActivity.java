85ac64e6f9183a02867cbcaee3d7331fef7661af
Sachin Rammoorthy <sachin.rammoorthy@gmail.com>
2018-01-20 11:07:29 +0530

    Vision

---
+++ LanguageLearner/app/src/main/java/com/example/android/languagelearner/QuizActivity.java
@@ -0,0 +1,58 @@
+package com.example.android.languagelearner;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class QuizActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private static final String TAG = "MainActivity";
+    private static final int RECORD_REQUEST_CODE = 101;
+    private static final int CAMERA_REQUEST_CODE = 102;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        Menu menu = navigation.getMenu();
+        MenuItem menuItem = menu.getItem(2);
+        menuItem.setChecked(true);
+
+        navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()) {
+                    case R.id.camera:
+                        takePicture();
+                        break;
+                    case R.id.navigation_dashboard:
+                        Intent intent = new Intent(QuizActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        break;
+                    case R.id.navigation_quiz:
+                        break;
+
+                }
+
+                return false;
+            }
+        });
+    }
+
+    public void takePicture() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, CAMERA_REQUEST_CODE);
+    }
+
+}
